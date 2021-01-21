7f689862a3e3affc5b02fdf9d394e684142a625b
Divyajeet9296 <73481267+Divyajeet9296@users.noreply.github.com>
2021-01-21 18:06:52 +0530

    Add files via upload

---
+++ Monster.js
@@ -0,0 +1,25 @@
+class Monster{
+    constructor(x, y, width, height){
+        var options={
+            isStatic=true,
+            restitution:0.5,
+            friction:1.0,
+
+        }
+        this.body=Bodies.rectangle(x, y, width, height, options);
+        this.width=width;
+        this.height= height;
+        this.image=loadImage("images/Monster-01.png");
+        World.add(world, this.body);
+    
+    display();
+
+
+    push();
+    translate(pos.x, pos.y);
+    rotate(angle);
+    rectMode(CENTER);
+    image(this.image,0,0,this.width,this.height);
+    pop();
+    }
+}
